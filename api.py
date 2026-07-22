c8090d791e47f167fab3b2378679feb60585a946
YuriNeves148 <git.hub.yuri.148@gmail.com>
2026-07-22 18:36:58 -0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,241 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+def conecta_banco():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        database="proj_cartao",
+        password="12345678"
+    )
+
+@app.route("/home/lista_historico")
+def lista_historico():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    sql = "select pessoa.nome as nome_pessoa, banco.nome as nome_banco, lojasite.nome as onde, " \
+    "compra.data_compra, compra.valor_total, qtd_parcela, (compra.valor_total / compra.qtd_parcela) as valor_parcela " \
+    "from compra join pessoa on pessoa.id_pessoa = compra.id_pessoa " \
+    "join banco on compra.id_banco = banco.id_banco " \
+    "join lojasite on compra.id_lojasite = lojasite.id_lojasite " \
+    "join parcela on compra.id_compra = parcela.id_parcela order by data_compra asc;"
+
+    cursor.execute(sql)
+    dado = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify(dado), 200    
+
+@app.route("/home/faturas")
+def fatura_nubank():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    sql_nu = "select SUM(valor_total/qtd_parcela) as fat_nu from compra where id_banco = 1;"
+    cursor.execute(sql_nu)
+    fat_nu = cursor.fetchone()
+    #print("FATURA NU:", fat_nu)
+
+    sql_bb = "select SUM(valor_total/qtd_parcela) as fat_nu from compra where id_banco = 2;" #simulando os valores da C6
+    cursor.execute(sql_bb)
+    fat_bb = cursor.fetchone()
+    #print("FATURA BB:", fat_bb)
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify({"fat_bb": fat_bb, "fat_nu": fat_nu})
+
+@app.route("/criacao/adicionar/pessoa", methods=["POST"])
+def adiciona_pessoa():
+    dado = request.get_json()
+    nome = dado.get("nome")
+    #print("NOME:", nome)
+    conexao = conecta_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT INTO pessoa (nome) VALUES (%s)", (nome,))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+    
+    return jsonify({"nome":nome})
+
+@app.route("/criacao/adicionar/banco", methods=["POST"])
+def adiciona_banco():
+    dado = request.get_json()
+    nome = dado.get("nome")
+    #print("\n\n\nnome: ", nome)
+    conexao = conecta_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT INTO banco (nome) VALUES (%s)", (nome,))
+    conexao.commit()
+    
+    cursor.close()
+    conexao.close()
+    
+    linhasAfetadas = cursor.rowcount
+
+    if (linhasAfetadas == 0):
+        return jsonify({"erro": "erro ao adicionar banco"})
+    
+    return jsonify(nome)
+    
+@app.route("/criacao/adicionar/loja", methods=["POST"])
+def adiciona_loja():
+    dado = request.get_json()
+    nome = dado.get("nome")
+
+    conexao = conecta_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("INSERT INTO lojasite (nome) VALUES (%s)", (nome,))
+    conexao.commit()
+
+    linhasAfetadas = cursor.rowcount
+
+    cursor.close()
+    conexao.close()
+
+    if linhasAfetadas == 0:
+        return jsonify({"erro":"nao foi possivel inserir nome."})
+    
+    return jsonify(nome)
+
+@app.route("/criacao/lista_pessoa")
+def lista_pessoa():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT nome FROM pessoa")
+    dado = cursor.fetchall()
+    #print(dado)
+    cursor.close()
+    conexao.close()
+
+    return jsonify(dado)
+
+@app.route("/criacao/lista_banco")
+def lista_banco():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT nome FROM banco")
+    dado = cursor.fetchall()
+    #print(dado)
+    cursor.close()
+    conexao.close()
+
+    return jsonify(dado)
+
+@app.route("/criacao/lista_loja")
+def lista_loja():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT nome FROM lojasite")
+    dado = cursor.fetchall()
+    #print(dado)
+    cursor.close()
+    conexao.close()
+
+    return jsonify(dado)
+
+
+@app.route("/compra/lista")
+def lista_compra():
+    conexao = conecta_banco()
+    cursor = conexao.cursor(dictionary=True)
+
+    sql = "select compra.id_compra, pessoa.nome as nome_pessoa, banco.nome as nome_banco, " \
+    "lojasite.nome as onde, compra.data_compra, compra.valor_total, qtd_parcela, " \
+    "(compra.valor_total / compra.qtd_parcela) as valor_parcela " \
+    "from compra join pessoa on pessoa.id_pessoa = compra.id_pessoa " \
+    "join banco on compra.id_banco = banco.id_banco " \
+    "join lojasite on compra.id_lojasite = lojasite.id_lojasite " \
+    "join parcela on compra.id_compra = parcela.id_parcela " \
+    "group by compra.id_compra;"
+
+    cursor.execute(sql)
+    dados = cursor.fetchall()
+    #print(dados)
+    return jsonify(dados)
+
+@app.route("/compra/salvar", methods=["POST"])
+def salvar_compra():
+    dados = request.get_json()
+    data = dados.get("data_comp")
+    nome = dados.get("nome_comp")
+    banco = dados.get("banco_comp")
+    loja = dados.get("loja_comp")
+    valor_total = dados.get("valor_total_comp")
+    qtd_parcela = dados.get("qtd_parcela_comp")
+
+    conexao = conecta_banco()
+    cursor = conexao.cursor()
+
+    # encontrando id do nome
+    sql_id_nome = "SELECT id_pessoa FROM pessoa WHERE nome = %s"
+    cursor.execute(sql_id_nome, (nome,))
+    id_pessoa = cursor.fetchone()[0]
+    #print("nome pessoa: ", nome)
+    #print("id pessoa: ", id_pessoa)
+
+    # encontrando id do banco
+    sql_id_banco = "SELECT id_banco FROM banco WHERE nome = %s"
+    cursor.execute(sql_id_banco, (banco,))
+    id_banco = cursor.fetchone()[0]
+    #print("nome banco: ", banco)
+    #print("id banco: ", id_banco)
+
+    # encontrando id da loja
+    sql_id_loja = "SELECT id_lojasite FROM lojasite WHERE nome = %s"
+    cursor.execute(sql_id_loja, (loja,))
+    id_loja = cursor.fetchone()[0]
+    #print("nome loja: ", loja)
+    #print("id loja: ", id_loja)
+
+    sql_salvar = "INSERT INTO compra " \
+    "(id_pessoa, id_banco, id_lojasite, data_compra, valor_total, qtd_parcela) VALUES" \
+    "(%s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql_salvar, (id_pessoa, id_banco, id_loja, data, valor_total, qtd_parcela))
+    conexao.commit()
+
+    print("valores salvos")
+    
+    cursor.close()
+    conexao.close()
+
+    return jsonify(success=True, message="Compra adicionada!"), 200
+
+@app.route("/compra/excluir", methods=["DELETE"])
+def exclui_compra():
+    dados = request.get_json()
+    codigo = dados.get("condigo_comp")
+    
+    conexao = conecta_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("DELETE FROM parcela WHERE id_compra = %s", (codigo,))
+    cursor.execute("DELETE FROM compra WHERE id_compra = %s", (codigo,))
+    conexao.commit()
+
+    return jsonify({"success":"elemento excluido"}), 200
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
---
+++ script.js
@@ -0,0 +1,399 @@
+function mostrar(id) {
+  document.querySelectorAll(".pagina").forEach((secao) => {
+    secao.classList.remove("ativa");
+  });
+  document.getElementById(id).classList.add("ativa");
+}
+
+const inputdata = document.getElementById("data");
+const mes = document.getElementById("mes");
+
+const meses = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+const mesAtual = new Date().getMonth();
+const mesSelecionado = document.getElementById("escolha_mes");
+mesSelecionado.value = mesAtual;
+
+// requisicoes
+const api_url = "http://127.0.0.1:5000";
+
+const listaa_historico = document.getElementById("lista_historico");
+const valor_fatura_nubank = document.getElementById("valor_nubank");
+const valor_fatura_c6 = document.getElementById("valor_c6");
+
+// adicionar pessoa
+const input_pessoa = document.getElementById("input_pessoa");
+const btn_adicionar_pessoa = document.getElementById("btn_adicionar_pessoa");
+// adicionar banco
+const input_banco = document.getElementById("input_banco");
+const btn_adicionar_banco = document.getElementById("btn_adicionar_banco");
+// adicionar loja
+const btn_adicionar_loja = document.getElementById("btn_adicionar_loja");
+const input_loja = document.getElementById("input_loja");
+
+// lista área CRIAÇÃO (cont = conteudo)
+const lista_pessoa_cont = document.getElementById("lista_pessoa_cont");
+const lista_banco_cont = document.getElementById("lista_banco_cont");
+const lista_loja_cont = document.getElementById("lista_loja_cont");
+
+// lista área COMPRA
+const lista_compra_cont = document.getElementById("hist_lista_cont");
+const salvar_compra = document.getElementById("salvar_compra");
+const codigo = document.getElementById("nav_codigo");
+const data = document.getElementById("nav_data");
+const nome = document.getElementById("nav_quem");
+const banco = document.getElementById("nav_banco");
+const loja = document.getElementById("nav_loja");
+const valor_total = document.getElementById("nav_valor_total");
+const qtd_parcela = document.getElementById("nav_parcela");
+let idEditandoCompra = null;
+let dataEditandoCompra = null;
+let nomeEditandoCompra = null;
+let bancoEditandoCompra = null;
+let lojaEditandoCompra = null;
+let valor_totalEditandoCompra = null;
+let qtd_parcelaEditandoCompra = null;
+const btn_excluir_compra = document.getElementById("btn_excluir_compra");
+const btn_apagar_inputs = document.getElementById("btn_apagar_inputs");
+
+async function lista_historico() {
+  const resposta = await fetch(`${api_url}/home/lista_historico`);
+
+  if (!resposta.ok) {
+    alert("Erro ao acessar ao exibir a lista.");
+  }
+  const dado = await resposta.json();
+  listaa_historico.innerHTML = "";
+
+  dado.forEach((item) => {
+    const dataa = new Date(item.data_compra);
+    const dataFormato = dataa.toLocaleDateString("pt-BR");
+    const novo_item = document.createElement("li");
+    novo_item.innerHTML = `
+    <span class="data">${dataFormato}</span>
+    <span class="nome">${item.nome_pessoa}</span>
+    <span class="banco">${item.nome_banco}</span>
+    <span class="onde">${item.onde}</span>
+    <span class="valor">${parseFloat(item.valor_total).toFixed(2)}</span>
+    <span class="valor">${item.qtd_parcela}</span>
+    <span class="valor">${parseFloat(item.valor_parcela).toFixed(2)}</span>
+
+`;
+
+    listaa_historico.appendChild(novo_item);
+  });
+}
+
+async function faturas() {
+  const resposta = await fetch(`${api_url}/home/faturas`);
+
+  if (!resposta.ok) {
+    alert("Não foi possível exibir fatura da nubank.");
+  }
+
+  const dado = await resposta.json();
+  valor_fatura_nubank.innerHTML = "";
+  valor_fatura_c6.innerHTML = "";
+
+  // mas que porra aconteceu aqui?
+  //console.log("fatura nubank: ", dado.fat_nu.fat_nu);
+  //console.log("fatura bb: ", dado.fat_bb.fat_nu);
+
+  valor_fatura_nubank.innerHTML = `${parseFloat(dado.fat_nu.fat_nu).toFixed(2)}`;
+  valor_fatura_c6.innerHTML = `${parseFloat(dado.fat_bb.fat_nu).toFixed(2)}`;
+}
+
+btn_adicionar_pessoa.addEventListener("click", async () => {
+  const nome_pessoa = input_pessoa.value.trim();
+  if (nome_pessoa === "") {
+    alert("Digite um nome antes de salvar!");
+    return;
+  }
+  try {
+    const resposta = await fetch(`${api_url}/criacao/adicionar/pessoa`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nome: nome_pessoa }),
+    });
+
+    if (!resposta.ok) {
+      alert("Falha ao salvar nome de pessoa");
+    }
+    const dado = await resposta.json();
+    console.log("nome adicionado: ", dado);
+    input_pessoa.value = "";
+  } catch (erro) {
+    alert("Erro ao conectar com o banco");
+  }
+});
+
+btn_adicionar_banco.addEventListener("click", async () => {
+  nome_banco = input_banco.value.trim();
+  if (nome_banco === "") {
+    alert("digite o nome do banco para salvar!");
+    return;
+  }
+  try {
+    const resposta = await fetch(`${api_url}/criacao/adicionar/banco`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nome: nome_banco }),
+    });
+    if (!resposta.ok) {
+      alert("Nao foi possivel salvar o nome do banco!");
+    }
+    const dado = await resposta.json();
+    console.log("Banco adicionado: ", dado);
+    input_banco.value = "";
+  } catch (erro) {
+    alert("erro ao acesso a API (adicionar banco).");
+  }
+});
+
+btn_adicionar_loja.addEventListener("click", async () => {
+  nome_loja = input_loja.value.trim();
+  if (nome_loja === "") {
+    alert("Digite o nome da loja antes de salvar!");
+    return;
+  }
+
+  try {
+    const resposta = await fetch(`${api_url}/criacao/adicionar/loja`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nome: nome_loja }),
+    });
+    if (!resposta.ok) {
+      alert("Erro ao acessar ao salvar loja.");
+      return;
+    }
+    const dado = await resposta.json();
+    console.log("loja adicionada: ", dado);
+    input_banco.value = "";
+  } catch (erro) {
+    alert("Erro ao acessar a API (salvar loja).");
+  }
+});
+
+async function lista_pessoa_func() {
+  const resposta = await fetch(`${api_url}/criacao/lista_pessoa`);
+  if (!resposta.ok) {
+    alert("Erro ao acessar a API (lista pessoa)");
+    return;
+  }
+  lista_pessoa_cont.innerHTML = "";
+  dado = await resposta.json();
+  dado.forEach((secao) => {
+    const novo_item = document.createElement("li");
+    novo_item.textContent = `${secao.nome}`; // nome da coluna no banco
+    lista_pessoa_cont.appendChild(novo_item);
+  });
+}
+
+async function lista_banco_func() {
+  const resposta = await fetch(`${api_url}/criacao/lista_banco`);
+  if (!resposta.ok) {
+    alert("Nao foi possivel conetar a API (lista banco).");
+    return;
+  }
+  const dado = await resposta.json();
+  lista_banco_cont.value = "";
+  dado.forEach((secao) => {
+    const novo_item = document.createElement("li");
+    novo_item.textContent = `${secao.nome}`;
+    lista_banco_cont.append(novo_item);
+  });
+}
+
+async function lista_loja_func() {
+  const resposta = await fetch(`${api_url}/criacao/lista_loja`);
+  if (!resposta.ok) {
+    alert("Nao foi possivel conetar a API (lista loja).");
+    return;
+  }
+  const dado = await resposta.json();
+  lista_loja_cont.value = "";
+  //console.log(dado);
+  dado.forEach((secao) => {
+    const novo_item = document.createElement("li");
+    novo_item.textContent = `${secao.nome}`;
+    lista_loja_cont.append(novo_item);
+  });
+}
+
+// área COMPRA
+async function historico_compra() {
+  const resposta = await fetch(`${api_url}/compra/lista`);
+
+  if (!resposta.ok) {
+    alert("Erro ao acessar ao exibir a lista (compra).");
+  }
+  const dado = await resposta.json();
+  lista_compra_cont.innerHTML = "";
+
+  dado.forEach((item) => {
+    const dataa = new Date(item.data_compra);
+    const dataFormato = dataa.toLocaleDateString("pt-BR");
+    const valor_parcela = `${parseFloat(item.valor_parcela).toFixed(2)}`;
+    const novo_item = document.createElement("li");
+    novo_item.innerHTML = `
+    <span class="codigo">${item.id_compra}.</span>
+    <span class="data">${dataFormato}</span>
+    <span class="nome">${item.nome_pessoa}</span>
+    <span class="banco">${item.nome_banco}</span>
+    <span class="onde">${item.onde}</span>
+    <span class="valor">${item.valor_total}</span>
+    <span class="valor">${item.qtd_parcela}</span>
+    <span class="valor">${valor_parcela}</span>
+`;
+    novo_item.dataset.id = item.id_compra;
+    novo_item.addEventListener("click", () => {
+      codigo.value = item.id_compra;
+      console.log("a", novo_item.id_compra);
+      const dataInput = dataa.toISOString().split("T")[0];
+      data.value = dataInput;
+      nome.value = item.nome_pessoa;
+      banco.value = item.nome_banco;
+      loja.value = item.onde;
+      valor_total.value = item.valor_total;
+      qtd_parcela.value = item.qtd_parcela;
+
+      idEditandoCompra = item.id_compra;
+      dataEditandoCompra = dataFormato;
+      nomeEditandoCompra = item.nome_pessoa;
+      bancoEditandoCompra = item.nome_banco;
+      lojaEditandoCompra = item.onde;
+      valor_totalEditandoCompra = item.valor_total;
+      qtd_parcelaEditandoCompra = item.qtd_parcela;
+    });
+    lista_compra_cont.appendChild(novo_item);
+  });
+}
+
+salvar_compra.addEventListener("click", async () => {
+  valor_data = data.value.trim();
+  valor_nome = nome.value.trim();
+  valor_banco = banco.value.trim();
+  valor_loja = loja.value.trim();
+  valor_valor_total = valor_total.value.trim();
+  valor_qtd_parcela = qtd_parcela.value.trim();
+  if (
+    valor_data === "" ||
+    valor_nome === "" ||
+    valor_banco === "" ||
+    valor_loja === "" ||
+    valor_valor_total === "" ||
+    valor_qtd_parcela === ""
+  ) {
+    alert("Preencha todos os campos para salvar");
+    return;
+  }
+
+  try {
+    const resposta = await fetch(`${api_url}/compra/salvar`, {
+      method: ["POST"],
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data_comp: valor_data,
+        nome_comp: valor_nome,
+        banco_comp: valor_banco,
+        loja_comp: valor_loja,
+        valor_total_comp: valor_valor_total,
+        qtd_parcela_comp: valor_qtd_parcela,
+      }),
+    });
+
+    if (!resposta.ok) {
+      alert("Nao foi possível salvar a compra.");
+      return;
+    }
+    data.value = "";
+    nome.value = "";
+    banco.value = "";
+    loja.value = "";
+    valor_total.value = "";
+    qtd_parcela.value = "";
+    const dado = await resposta.json();
+    console.log(dado);
+  } catch (erro) {
+    alert("Nao foi possivel se conectar a API");
+  }
+});
+
+btn_excluir_compra.addEventListener("click", async () => {
+  valor_codigo = codigo.value.trim();
+  valor_data = data.value.trim();
+  valor_nome = nome.value.trim();
+  valor_banco = banco.value.trim();
+  valor_loja = loja.value.trim();
+  valor_valor_total = valor_total.value.trim();
+  valor_qtd_parcela = qtd_parcela.value.trim();
+  if (
+    valor_codigo.value === "" ||
+    valor_data.value === "" ||
+    valor_nome.value === "" ||
+    valor_banco.value === "" ||
+    valor_loja.value === "" ||
+    valor_valor_total.value === "" ||
+    valor_qtd_parcela.value === ""
+  ) {
+    alert("Preencha todos os campos para salvar a compra.");
+    return;
+  }
+
+  try {
+    const resposta = await fetch(`${api_url}/compra/excluir`, {
+      method: ["DELETE"],
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        condigo_comp: valor_codigo,
+        data_comp: valor_data,
+        nome_comp: valor_nome,
+        banco_comp: valor_banco,
+        loja_comp: valor_loja,
+        valor_total_comp: valor_valor_total,
+        qtd_parcela_comp: valor_qtd_parcela,
+      }),
+    });
+
+    if (!resposta.ok) {
+      alert("Nao foi possível salvar a compra.");
+      return;
+    }
+    codigo.value = "";
+    data.value = "";
+    nome.value = "";
+    banco.value = "";
+    loja.value = "";
+    valor_total.value = "";
+    qtd_parcela.value = "";
+    alert("compra excluida com sucesso");
+    const dado = await resposta.json();
+    console.log(dado);
+  } catch (erro) {
+    alert("Nao foi possivel se conectar a API");
+  }
+});
+
+btn_apagar_inputs.addEventListener("click", () => {
+  codigo.value = "";
+  data.value = "";
+  nome.value = "";
+  banco.value = "";
+  loja.value = "";
+  valor_total.value = "";
+  qtd_parcela.value = "";
+});
