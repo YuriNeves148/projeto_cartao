5abc5bc08b19eca84610c69b2981ffaf35311439
YuriNeves148 <gut.hub.yuri.148@gmail.com>
2026-08-05 20:19:15 -0300

    banco de dados primario

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -11,7 +11,7 @@ def conecta_banco():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        database="proj_cartao4",
+        database="projeto_cartao",
         password="12345678"
     )
 
